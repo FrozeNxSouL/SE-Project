36ae4cf7bc8df8100fb91d64341812a77d684a76
Ratanon090 <Ratanon1@gmail.com>
2024-01-04 21:02:59 +0700

    sexy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 function HomePage() {
   
   return (
+    <>
     <div>HomePage</div>
-    
+    <div className='flex justify-center items-center w-80'>
+      <img src='https://cdn-prod.medicalnewstoday.com/content/images/articles/321/321428/couple-having-sex.jpg' />
+    </div>
+    </>
   )
 }
 
