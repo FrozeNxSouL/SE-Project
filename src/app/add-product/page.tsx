72daba523abe1b7d9f57a9e718cb2eaa29400178
Ratanon090 <Ratanon1@gmail.com>
2024-02-19 19:12:37 +0700

    done add-product-on-client

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,25 +1,10 @@
+"use client";
 import  prisma  from '@/lib/db/prisma';
 import { redirect } from 'next/navigation';
 import React from 'react'
+import addProduct from './add';
 
-async function addProduct(formData: FormData) {
-    "use server";
-    
-    const name = formData.get("name")?.toString();
-    const description =formData.get("description")?.toString();
-    const imageUrl = formData.get("imageUrl")?.toString();
-    const price = Number(formData.get("price") || 0)
 
-    if(!name || !description || !imageUrl || !price){
-        throw Error("Missing required fields");
-    }
-
-    await prisma.product.create({
-        data: {name,description,imageUrl,price},
-    });
-
-    redirect("/");
-}
 function AddProductPage() {
     return (
         <div className='p-4 max-w-7xl m-auto'>
