5a1eeb98ced3dbaf191f2771ccb94e9be0ff7a07
FrozeNxSouL <zenixzelon@hotmail.com>
2024-02-19 09:16:40 +0700

    basic content in homePage

---
+++ src/component/card/productCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function productCard() {
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    Shoes!
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline">Fashion</div>
+                    <div className="badge badge-outline">Products</div>
+                </div>
+            </div>
+        </div>
+    )
+}
