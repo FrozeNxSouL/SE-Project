9d95d1b5899772be721eb7fa97b9c1674329a95d
Ratanon090 <Ratanon1@gmail.com>
2024-02-19 18:30:31 +0700

    product and add-product

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,45 +1,49 @@
 import  prisma  from '@/lib/db/prisma';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-async function name(formData: FormData) {
-    "use server"
-    console.log(formData)
-    const a = await prisma.user.create({
-        data: {
-            name: "dadaw",
-            email: "wawd@gmail.com",
-            hashedPassword: "dawd",
-            address: "dawd",
-            phone: "dawdw",
-            score: 32,
-            picture: "dawd",
-        }
-    })
+async function addProduct(formData: FormData) {
+    "use server";
+    
+    const name = formData.get("name")?.toString();
+    const description =formData.get("description")?.toString();
+    const imageUrl = formData.get("imageUrl")?.toString();
+    const price = Number(formData.get("price") || 0)
+
+    if(!name || !description || !imageUrl || !price){
+        throw Error("Missing required fields");
+    }
+
+    await prisma.product.create({
+        data: {name,description,imageUrl,price},
+    });
+
+    redirect("/");
 }
 function AddProductPage() {
     return (
         <div className='p-4 max-w-7xl m-auto'>
             <h1 className='text-lg bg-purple-300 font-bold'>Add Product</h1>
-            <form action = {name}>
+            <form action = {addProduct}>
                 <input
-                    // required
+                    required
                     name="name"
                     placeholder="Name" className="input-bordered input mb-2 w-full"
                 />
                 <textarea
-                // required
+                required
                 name="description"
                 placeholder="Description"
                 className="textarea textarea-bordered mb-2 w-full"
                 />
                 <input
-                    // required
-                    name="imagUrl"
+                    required
+                    name="imageUrl"
                     type="url"
                     placeholder="Image URL" className="input-bordered input mb-2 w-full"
                 />
                 <input
-                    // required
+                    required
                     name="price"
                     placeholder="Price"
                     type="number"
