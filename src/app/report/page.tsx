9044835bc8d3027a9d612ed3ac42d7948a62d726
Thitiphatx <thitiphat6391@gmail.com>
2024-02-25 18:55:35 +0700

    ขโมยผลงาน report

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -8,55 +8,41 @@ export default function report() {
 
     return (
 
-        <div className='flex bg-base-100 p-6 justify-center'>
+        <div className='flex bg-base-100 p-6 justify-center max-w-screen-lg mx-auto'>
 
-            <div className='w-1/2 border-[2px] flex bg-base-100 p-6 flex-col'>
-
-
-                <div className='flex'>
-
-                    <h1>รายงาน</h1>
-
-
-                    <h1 className='overflow-visible h-6 font-extrabold ml-4 '>paradornnnnnnnnnnnnnnnnnnnnnnnnnnnnn</h1>
-
-
-
-                    <div className="h-11 avatar ml-5">
-                        <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <div className='w-full shadow-lg flex flex-col bg-base-100 p-6'>
+                <h1>รายงาน</h1>
+                <div className="flex flex-row flex-wrap justify-between gap-4">
+                    <div className="flex justify-start m-5">
+                        <div className="h-11 avatar">
+                            <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
                         </div>
+                        <h1 className='overflow-visible h-6 font-extrabold ml-4 '>wow</h1>
                     </div>
-
-                    <div className="ml-7 collapse bg-base-200">
-                        <input type="checkbox" className="peer" />
-                        <div className="collapse-title bg-primary ">
-                            รายละเอียดสินค้า
-                        </div>
-                        <div className="collapse-content bg-primary ">
-                            {/* <p>ชื่อสินค้า : รองเท้า</p>
-                            <p>สถานะสินค้า : รอจัดส่ง</p>
-                            <p>เลขพัสดุ : 10111010110</p> */}
-                            <figure>
-                                <img className="object-cover w-full h-40" src="https://media.discordapp.net/attachments/1130011642361561209/1185824208140390441/IMG_2470.jpg?ex=65e4125a&is=65d19d5a&hm=7d6d5d4bcceeae4c4223cd0a430a95d0ec2e3d1bbcf2a2f028a927adc1661826&=&format=webp" />
-                            </figure>
-                            <div className="card-body p-5 bg-base-100">
-                                <div className="flex flex-wrap gap-2">
-                                    <div className="badge badge-primary">NEW</div>
-                                    <div className="badge badge-outline">wow</div>
-                                </div>
-                                <div className="card-title truncate overflow-hidden max-w-60">asdasd</div>
-                                <div className="card-actions justify-end">
-                                    <div>
-                                        <p className="text-primary text-xl">฿199</p>
+                    <div className='min-w-96 flex'>
+                        <div className="collapse bg-base-200">
+                            <input type="checkbox" className="peer" />
+                            <div className="collapse-title bg-primary ">
+                                รายละเอียดสินค้า
+                            </div>
+                            <div className="collapse-content bg-primary ">
+                                <figure>
+                                    <img className="object-cover w-full h-40" src="https://media.discordapp.net/attachments/1130011642361561209/1185824208140390441/IMG_2470.jpg?ex=65e4125a&is=65d19d5a&hm=7d6d5d4bcceeae4c4223cd0a430a95d0ec2e3d1bbcf2a2f028a927adc1661826&=&format=webp" />
+                                </figure>
+                                <div className="card-body p-5 bg-base-100">
+                                    <div className="card-title truncate overflow-hidden max-w-60">asdasd</div>
+                                    <div className="card-actions justify-end">
+                                        <div>
+                                            <p className="text-primary text-xl">฿199</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
-
                 <form className="form-control">
                     <label className="label cursor-pointer">
                         <span className="label-text">ขายสินค้าไม่ตรงปก</span>
@@ -74,18 +60,18 @@ export default function report() {
                     </label>
 
                     <label className="label cursor-pointer">
-                        <span className="label-text">ควยย</span>
+                        <span className="label-text">พูดจาไม่เข้าหู</span>
                         <input type="checkbox" className="checkbox checkbox-primary" />
                     </label>
 
 
 
                     <h1 className='font-extrabold mt-6'>เลือกรูปภาพ</h1>
-                    <input type="file" className="file-input file-input-bordered file-input-secondary w-full max-w-xs mt-2" />
+                    <input type="file" className="file-input file-input-bordered w-full mt-2" />
 
-                    <textarea className="h-40 textarea textarea-warning mt-4" placeholder="Bio"></textarea>
+                    <textarea className="h-40 textarea textarea-primary mt-4" placeholder="Bio"></textarea>
 
-                    <button className="btn btn-wide btn-primary mt-4 ">submit</button>
+                    <button className="btn btn-block btn-primary mt-4 ">submit</button>
 
                 </form>
 
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,176 +1,139 @@
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 export default function user() {
 
     return (
 
 
-        <div className='flex bg-base-100 p-6'>
+        <div className='flex bg-base-100 p-6 max-w-screen-xl mx-auto'>
 
 
-            <div className='flex flex-col items-center w-1/4'>
+            <div className='flex flex-col items-center'>
 
 
                 <div className="avatar ">
-                    <div className='w-[15rem] rounded-full'>
+                    <div className='w-40 rounded-full'>
                         <img src='https://media.discordapp.net/attachments/1130011642361561209/1185824208140390441/IMG_2470.jpg?ex=65e4125a&is=65d19d5a&hm=7d6d5d4bcceeae4c4223cd0a430a95d0ec2e3d1bbcf2a2f028a927adc1661826&=&format=webp&width=507&height=676'>
                         </img>
                     </div>
 
                 </div>
-                <h1 className='text-3xl text-orange-600 mt-5 font-mono' >
-                    paradorn
-                </h1>
+                <h1 className='font-bold text-2xl mt-5'>paradorn</h1>
 
-                <div className='w-72 h-72 border-[2px] mt-5 rounded-2xl text-center'>
-                    <h1 className='text-2xl text-orange-600 mt-5 text-center font-mono' >
-                        คะแนน : 0
-                    </h1>
+                <div className='w-72 h-72 mt-5 rounded-2xl text-center'>
 
                     <div className="rating rating-md">
-                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
+                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-primary" />
+                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-primary" />
+                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-primary" checked/>
+                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-primary" />
+                        <input type="radio" name="rating-7" className="mask mask-star-2 bg-primary" />
                     </div>
 
-                    <div className="stats bg-primary text-primary-content mt-3">
+                    <div className="stats text-primary-content mt-3">
                         <div className="stat">
-                            <div className="stat-title  ">Current balance</div>
-                            <div className="stat-value">89,400 บาท</div>
+                            <div className="stat-title">Current balance</div>
+                            <div className="stat-value text-primary">89,400 บาท</div>
                             <div className="stat-actions">
                                 <button className="btn btn-sm">Withdrawal</button>
                                 <button className="btn btn-sm ml-3">deposit</button>
                             </div>
                         </div>
-
                     </div>
 
                 </div>
-
-
             </div>
 
             <div className='flex flex-col w-3/4'>
-                {/* <button className="btn btn-primary ">สถานะสินค้า</button>
-                    <button className="btn btn-primary ml-10">สถานะสินค้า</button> */}
                 <div role="tablist" className="tabs tabs-bordered ">
                     <a role="tab" className="tab">สถานะสินค้า</a>
                     <a role="tab" className="tab tab-active">สินค้าที่มี</a>
                     <a role="tab" className="tab">ประวัติการซื้อ-ขาย</a>
                 </div>
-                <div className='bg-gradient-to-r from-purple-500 to-pink-500 h-full p-10 grid grid-cols-5 mx-auto gap-9' >
-
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl ">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className='flex flex-wrap p-3 mx-auto gap-2' >
+                    <div className="card bg-base-100 shadow-xl basis-72 transition cursor-pointer hover:scale-[1.01] min-w-60">
+                        <figure>
+                            <img className="object-cover w-full h-40" src="https://media.discordapp.net/attachments/1130011642361561209/1185824208140390441/IMG_2470.jpg?ex=65e4125a&is=65d19d5a&hm=7d6d5d4bcceeae4c4223cd0a430a95d0ec2e3d1bbcf2a2f028a927adc1661826&=&format=webp" />
+                        </figure>
+                        <div className="card-body p-5 bg-base-100">
+                            <div className="flex flex-wrap gap-2">
+                            <div className="badge badge-primary">NEW</div>
+                            <div className="badge badge-outline">wow</div>
+                            </div>
+                            <div className="card-title truncate overflow-hidden max-w-60">asdasd</div>
                             <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                            <div>
+                                <p className="text-primary text-xl">฿199</p>
+                            </div>
                             </div>
                         </div>
                     </div>
-
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl ">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card bg-base-100 shadow-xl basis-72 transition cursor-pointer hover:scale-[1.01] min-w-60">
+                        <figure>
+                            <img className="object-cover w-full h-40" src="https://media.discordapp.net/attachments/1130011642361561209/1185824208140390441/IMG_2470.jpg?ex=65e4125a&is=65d19d5a&hm=7d6d5d4bcceeae4c4223cd0a430a95d0ec2e3d1bbcf2a2f028a927adc1661826&=&format=webp" />
+                        </figure>
+                        <div className="card-body p-5 bg-base-100">
+                            <div className="flex flex-wrap gap-2">
+                            <div className="badge badge-primary">NEW</div>
+                            <div className="badge badge-outline">wow</div>
+                            </div>
+                            <div className="card-title truncate overflow-hidden max-w-60">asdasd</div>
                             <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                            <div>
+                                <p className="text-primary text-xl">฿199</p>
+                            </div>
                             </div>
                         </div>
                     </div>
-
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl ">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card bg-base-100 shadow-xl basis-72 transition cursor-pointer hover:scale-[1.01] min-w-60">
+                        <figure>
+                            <img className="object-cover w-full h-40" src="https://media.discordapp.net/attachments/1130011642361561209/1185824208140390441/IMG_2470.jpg?ex=65e4125a&is=65d19d5a&hm=7d6d5d4bcceeae4c4223cd0a430a95d0ec2e3d1bbcf2a2f028a927adc1661826&=&format=webp" />
+                        </figure>
+                        <div className="card-body p-5 bg-base-100">
+                            <div className="flex flex-wrap gap-2">
+                            <div className="badge badge-primary">NEW</div>
+                            <div className="badge badge-outline">wow</div>
+                            </div>
+                            <div className="card-title truncate overflow-hidden max-w-60">asdasd</div>
                             <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                            <div>
+                                <p className="text-primary text-xl">฿199</p>
+                            </div>
                             </div>
                         </div>
                     </div>
-
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl ">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card bg-base-100 shadow-xl basis-72 transition cursor-pointer hover:scale-[1.01] min-w-60">
+                        <figure>
+                            <img className="object-cover w-full h-40" src="https://media.discordapp.net/attachments/1130011642361561209/1185824208140390441/IMG_2470.jpg?ex=65e4125a&is=65d19d5a&hm=7d6d5d4bcceeae4c4223cd0a430a95d0ec2e3d1bbcf2a2f028a927adc1661826&=&format=webp" />
+                        </figure>
+                        <div className="card-body p-5 bg-base-100">
+                            <div className="flex flex-wrap gap-2">
+                            <div className="badge badge-primary">NEW</div>
+                            <div className="badge badge-outline">wow</div>
+                            </div>
+                            <div className="card-title truncate overflow-hidden max-w-60">asdasd</div>
                             <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                            <div>
+                                <p className="text-primary text-xl">฿199</p>
+                            </div>
                             </div>
                         </div>
                     </div>
-
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                                        <div className="card bg-base-100 shadow-xl basis-72 transition cursor-pointer hover:scale-[1.01] min-w-60">
+                        <figure>
+                            <img className="object-cover w-full h-40" src="https://media.discordapp.net/attachments/1130011642361561209/1185824208140390441/IMG_2470.jpg?ex=65e4125a&is=65d19d5a&hm=7d6d5d4bcceeae4c4223cd0a430a95d0ec2e3d1bbcf2a2f028a927adc1661826&=&format=webp" />
+                        </figure>
+                        <div className="card-body p-5 bg-base-100">
+                            <div className="flex flex-wrap gap-2">
+                            <div className="badge badge-primary">NEW</div>
+                            <div className="badge badge-outline">wow</div>
+                            </div>
+                            <div className="card-title truncate overflow-hidden max-w-60">asdasd</div>
                             <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                            <div>
+                                <p className="text-primary text-xl">฿199</p>
+                            </div>
                             </div>
                         </div>
                     </div>
-
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card card-compact w-60 h-80 bg-base-100 shadow-xl">
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                        <div className="card-body">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-                            <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
-                            </div>
-                        </div>
-                    </div>
-
-
 
                 </div>
 
