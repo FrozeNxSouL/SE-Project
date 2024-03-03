7108f80d17c9ecda87b64f021851d6ea641e4e00
paradorn jancharoen <paradorn657@gmail.com>
2024-03-03 18:07:32 +0700

    jjj

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,6 +1,399 @@
+import { useEffect } from 'react';
+
 
 export default function payment() {
-    return (
-      <div>paymentPage</div>
-    )
+
+  
+
+
+  return (
+
+
+
+
+
+    <div className='flex'>
+
+    
+    
+
+    <div className='w-2/3'>
+
+    
+    
+    <div className="bg-white shop w-full" >
+      <div className="ml-6">
+        <div className="p-2">
+          <div id="checkout-left" className="flex">
+            <label id="checkbox">
+              <span>
+                <input className="w-[16px] h-[16px]" type="checkbox">
+
+                </input>
+              </span>
+
+            </label>
+
+            <div id="shop title " className="flex ml-3">
+
+              <img className="h-[20px]" src="https://lzd-img-global.slatic.net/g/tps/imgextra/i2/O1CN01GlrQiX1rbI98A7PB1_!!6000000005649-2-tps-94-36.png_2200x2200q80.png_.webp">
+              </img>
+              <span className="ml-2">paradorn shop</span>
+
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+
+      <div className="border product flex ">
+        <div className="left flex ml-6">
+
+          <div className="flex p-2 ">
+            <label >
+              <input className="w-[16px] h-[16px]" type="checkbox"></input>
+            </label>
+
+            <div className="wrap-img">
+              <img className="w-[80px] h-[80px] ml-4" src="https://lzd-img-global.slatic.net/g/p/2f94114819272a86170d25eacb501e9a.jpg_2200x2200q80.jpg_.webp"></img>
+            </div>
+
+          </div>
+
+          <div className="content ml-5 p-2">
+            <a>เสื้อครอปเอวลอย แขนสั้น คอกลม สกรีนลาย สำหรับผู้หญิง เสื้อครอปเเขนสั้นคอกลม เสื้อครอปมินิมอล</a>
+            <br />
+            <a>No Brand, Color Family:White, Variation2:2XL</a>
+
+          </div>
+        </div>
+
+        <div className="middle m-auto mt-2">
+
+          <div className="current_price ">
+            <p className="current-price bg-red-700">฿53.00</p>
+
+          </div>
+
+          <div className="operations mt-3">
+            <span className="wishlist">
+              <span className="material-icons">favorite</span>
+            </span>
+
+            <span className="delete ml-1">
+              <span className="material-icons">delete</span>
+            </span>
+          </div>
+        </div>
+
+
+        <div className="right  m-auto ">
+          <div className="quantity ">
+            <div className="next-number-picker-handler-wrap flex">
+
+              <div>
+                <span className="minus">
+                  <span className="material-icons bg-slate-400 justify-center">
+                    remove
+                  </span>
+                </span>
+
+              </div>
+
+
+              <div>
+                <span className="num">
+
+                  <input className="w-24 text-center " type="text" step="1" min="1" max="5" value="5" autoComplete="off" height="100%" >
+                  </input>
+
+
+                </span>
+              </div>
+
+              <div>
+                <span className="add">
+
+                  <span className="material-icons bg-slate-400 justify-center">
+                    add
+                  </span>
+                </span>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+
+
+
+
+      </div>
+    </div>
+
+    <div className="bg-white shop w-full" >
+      <div className="ml-6">
+        <div className="p-2">
+          <div id="checkout-left" className="flex">
+            <label id="checkbox">
+              <span>
+                <input className="w-[16px] h-[16px]" type="checkbox">
+
+                </input>
+              </span>
+
+            </label>
+
+            <div id="shop title " className="flex ml-3">
+
+              <img className="h-[20px]" src="https://lzd-img-global.slatic.net/g/tps/imgextra/i2/O1CN01GlrQiX1rbI98A7PB1_!!6000000005649-2-tps-94-36.png_2200x2200q80.png_.webp">
+              </img>
+              <span className="ml-2">paradorn shop</span>
+
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+
+      <div className="border product flex ">
+        <div className="left flex ml-6">
+
+          <div className="flex p-2 ">
+            <label >
+              <input className="w-[16px] h-[16px]" type="checkbox"></input>
+            </label>
+
+            <div className="wrap-img">
+              <img className="w-[80px] h-[80px] ml-4" src="https://lzd-img-global.slatic.net/g/p/2f94114819272a86170d25eacb501e9a.jpg_2200x2200q80.jpg_.webp"></img>
+            </div>
+
+          </div>
+
+          <div className="content ml-5 p-2">
+            <a>เสื้อครอปเอวลอย แขนสั้น คอกลม สกรีนลาย สำหรับผู้หญิง เสื้อครอปเเขนสั้นคอกลม เสื้อครอปมินิมอล</a>
+            <br />
+            <a>No Brand, Color Family:White, Variation2:2XL</a>
+
+          </div>
+        </div>
+
+        <div className="middle m-auto mt-2">
+
+          <div className="current_price ">
+            <p className="current-price bg-red-700">฿53.00</p>
+
+          </div>
+
+          <div className="operations mt-3">
+            <span className="wishlist">
+              <span className="material-icons">favorite</span>
+            </span>
+
+            <span className="delete ml-1">
+              <span className="material-icons">delete</span>
+            </span>
+          </div>
+        </div>
+
+
+        <div className="right  m-auto ">
+          <div className="quantity ">
+            <div className="next-number-picker-handler-wrap flex">
+
+              <div>
+                <span className="minus">
+                  <span className="material-icons bg-slate-400 justify-center">
+                    remove
+                  </span>
+                </span>
+
+              </div>
+
+
+              <div>
+                <span className="num">
+
+                  <input className="w-24 text-center " type="text" step="1" min="1" max="5" value="5" autoComplete="off" height="100%" >
+                  </input>
+
+
+                </span>
+              </div>
+
+              <div>
+                <span className="add">
+
+                  <span className="material-icons bg-slate-400 justify-center">
+                    add
+                  </span>
+                </span>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+
+
+
+
+      </div>
+    </div>
+
+    <div className="bg-white shop w-full" >
+      <div className="ml-6">
+        <div className="p-2">
+          <div id="checkout-left" className="flex">
+            <label id="checkbox">
+              <span>
+                <input className="w-[16px] h-[16px]" type="checkbox">
+
+                </input>
+              </span>
+
+            </label>
+
+            <div id="shop title " className="flex ml-3">
+
+              <img className="h-[20px]" src="https://lzd-img-global.slatic.net/g/tps/imgextra/i2/O1CN01GlrQiX1rbI98A7PB1_!!6000000005649-2-tps-94-36.png_2200x2200q80.png_.webp">
+              </img>
+              <span className="ml-2">paradorn shop</span>
+
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+
+      <div className="border product flex ">
+        <div className="left flex ml-6">
+
+          <div className="flex p-2 ">
+            <label >
+              <input className="w-[16px] h-[16px]" type="checkbox"></input>
+            </label>
+
+            <div className="wrap-img">
+              <img className="w-[80px] h-[80px] ml-4" src="https://lzd-img-global.slatic.net/g/p/2f94114819272a86170d25eacb501e9a.jpg_2200x2200q80.jpg_.webp"></img>
+            </div>
+
+          </div>
+
+          <div className="content ml-5 p-2">
+            <a>เสื้อครอปเอวลอย แขนสั้น คอกลม สกรีนลาย สำหรับผู้หญิง เสื้อครอปเเขนสั้นคอกลม เสื้อครอปมินิมอล</a>
+            <br />
+            <a>No Brand, Color Family:White, Variation2:2XL</a>
+
+          </div>
+        </div>
+
+        <div className="middle m-auto mt-2">
+
+          <div className="current_price ">
+            <p className="current-price bg-red-700">฿53.00</p>
+
+          </div>
+
+          <div className="operations mt-3">
+            <span className="wishlist">
+              <span className="material-icons">favorite</span>
+            </span>
+
+            <span className="delete ml-1">
+              <span className="material-icons">delete</span>
+            </span>
+          </div>
+        </div>
+
+
+        <div className="right  m-auto ">
+          <div className="quantity ">
+            <div className="next-number-picker-handler-wrap flex">
+
+              <div>
+                <span className="minus">
+                  <span className="material-icons bg-slate-400 justify-center">
+                    remove
+                  </span>
+                </span>
+
+              </div>
+
+
+              <div>
+                <span className="num">
+
+                  <input className="w-24 text-center " type="text" step="1" min="1" max="5" value="5" autoComplete="off" height="100%" >
+                  </input>
+
+
+                </span>
+              </div>
+
+              <div>
+                <span className="add">
+
+                  <span className="material-icons bg-slate-400 justify-center">
+                    add
+                  </span>
+                </span>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+
+
+
+
+      </div>
+    </div>
+    
+    </div>
+
+
+
+
+
+
+
+
+    <div className='right bg-slate-300 w-1/3'>
+
+
+    </div>
+
+
+    </div>
+
+    
+
+
+
+    
+    
+  )
 }
