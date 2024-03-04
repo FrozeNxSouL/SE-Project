889dd03ec251bf53b1945f1b5ec4f9af6deeb56c
Thitiphatx <thitiphat6391@gmail.com>
2024-03-04 20:06:40 +0700

    poon kub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import Link from "next/link";
 import {getProducts,getAuctionProduct} from '@/api/action/fetch';
-import Category from '@/component/panel/category';
 import LatestProducts from '@/component/panel/latestProduct';
 import AuctionProducts from '@/component/panel/auction';
+import getCategory from './action/getCategory';
+import CategoryList from '@/component/panel/category';
 // import getAuctionProduct from '@/api/action/getAuctionProduct';
 
 
 export default async function HomePage() {
   const auctionProduct = await getAuctionProduct();
   const products = await getProducts();
+  const category = await getCategory();
 
   return (
     <div className='mx-auto flex justify-center flex-col gap-5 bg-base-100 max-w-screen-xl px-32'>
-      <Category />
+      <CategoryList data={category} />
       <div className="divider text-2xl font-bold">End soon</div>
       <AuctionProducts data={auctionProduct}/>
       <div className="divider text-2xl font-bold">Latest Products</div>
