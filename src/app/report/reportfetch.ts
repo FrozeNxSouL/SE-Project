67f683b3bf1e0c74a9532e8dd360c0751db7201a
paradorn jancharoen <paradorn657@gmail.com>
2024-03-14 20:25:04 +0700

    editreportfecth

--- src/app/report/reportfetch.ts
+++ src/app/report/reportfetch.ts
@@ -14,7 +14,6 @@ export async function createReport(Des: string,reportSelection: string[]) {
             userId: "65e226c76685873d32bd9fcb"
         }
     });
-   
 }
 
 export async function getusername(searchid: string) {
@@ -39,55 +38,15 @@ export async function getusername(searchid: string) {
       }
 }
 
-export async function getproductprice(searchid: string) {
-    try{
-        const productpriceWithThisid = await prisma.product.findMany({
-            where: {
-              id:searchid
-            },
-          });
-          const productprice = productpriceWithThisid.map(product => product.price);
-          console.log(productprice);
-          return productprice
-    }
-    catch (error) {
-        // Handle errors
-        console.error('Error fetching user with product name:', error);
-        throw error;
-      }
-}
-
-
-export async function getproductname(searchid: string) {
-    try{
-        const productsImgWithThisid = await prisma.product.findMany({
-            where: {
-              id:searchid
-            },
-          });
-          const productname = productsImgWithThisid.map(product => product.name);
-          console.log(productname);
-          return productname
-    }
-    catch (error) {
-        // Handle errors
-        console.error('Error fetching user with product name:', error);
-        throw error;
-      }
-}
 
 export async function getproduct(searchid: string) {
     try{
-        const productsImgWithThisid = await prisma.product.findMany({
+        const products = await prisma.product.findFirst({
             where: {
               id:searchid
             },
           });
-
-          const productimg = productsImgWithThisid.map(product => product.imageUrl);
-
-          console.log(productimg);
-          return productimg
+          return products
     }
     catch (error) {
         // Handle errors
