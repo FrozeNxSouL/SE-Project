d787546ea9ea4161b3855599237c0099be2972e6
Thitiphatx <thitiphat6391@gmail.com>
2024-04-01 15:04:12 +0700

    Added product image

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -14,7 +14,7 @@ function AddProductPage() {
   const [price, setPrice] = useState(0);
   const [category, setCategory] = useState("");
   const [productImage, setProductImage] = useState<string[]>([]);
-  const [imageField, setImageField] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
 
   const handleName = (e: React.ChangeEvent<HTMLInputElement>)=> {
@@ -35,16 +35,6 @@ function AddProductPage() {
   const handleTime = (e: React.ChangeEvent<HTMLInputElement>)=> {
     setTime(e.target.value ?? null)
   }
-  const handleImageField = (e: React.ChangeEvent<HTMLInputElement>)=> {
-    setImageField(e.target.value)
-  }
-  const handleAddImage = ()=> {
-    if (imageField !== "") {
-      const arr: string[] = [...productImage];
-      arr.push(imageField);
-      setProductImage(arr)
-    }
-  }
   const handleRemoveImage = (idx: number)=> {
     const arr: string[] = productImage.filter((img, index) => index !== idx);
     setProductImage(arr)
@@ -61,22 +51,77 @@ function AddProductPage() {
       transactionId: null,
       userId: null
     }
-    // addProduct(newProduct, time)
+    addProduct(newProduct, time);
+  }
+  const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files; // Get the selected files
+    try {
+      applyImage(files);
+    } catch (e: any) {
+      setError(e.message);
+    }
+  };
+
+  const applyImage = (files: FileList | null)=> {
+    if (files) {
+      if (files.length > 5) {
+        throw new Error("Can't upload image more than 5");
+      }
+      const newFileDataUrls: string[] = [];
+      const readers: FileReader[] = [];
+      for (let i = 0; i < files.length; i++) {
+        console.log(files[i].size);
+        if (files[i].size > 200000) {
+          throw new Error(`Image number ${i+1} is too big (under 200kb)`);
+        }
+
+        const reader = new FileReader();
+        readers.push(reader);
+        reader.onload = (e) => {
+          if (e.target) {
+            const dataUrl = e.target.result as string;
+            newFileDataUrls.push(dataUrl);
+            
+            if (newFileDataUrls.length === files.length) {
+              setProductImage(newFileDataUrls);
+            }
+          }
+        };
+        reader.readAsDataURL(files[i]); // Read the file as a data URL
+      }
+    }
   }
 
   return (
       <div className="mx-auto max-w-screen-lg min-h-screen bg-base-100">
-        
-        <div className="cursor-pointer size-32 bg-base-200 p-2 ring-1 ring-primary rounded-md">
-          <input type="file" className="absolute size-32 opacity-0 cursor-pointer" accept="image/jpeg" multiple/>
-          <div className="w-full h-full z-1 flex flex-col justify-center items-center">
-            <button className="btn btn-outline btn-sm">Browse</button>
-            <span className="text-center">Drag and drop</span>
-          </div>
-        </div>
-
         <div className="w-full p-10 space-y-2">
+          {error && (
+              <div role="alert" className="alert">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                  <span>{error}</span>
+                  <div>
+                      <button className="btn btn-error btn-sm" onClick={()=> {setError(null)}}><span className="material-icons">close</span></button>
+                  </div>
+              </div>
+          )}
           <div className="divider text-xl font-bold">Create new product</div>
+          <div className="flex flex-wrap justify-center gap-3">
+              <div className="cursor-pointer size-32 bg-base-200 transition hover:ring-1 hover:ring-primary rounded-md">
+                <input type="file" onChange={handleFileSelect} className="absolute size-32 opacity-0 cursor-pointer" accept="image/jpeg" multiple/>
+                <div className="w-full h-full z-1 flex flex-col justify-center items-center">
+                  <button className="btn btn-outline btn-sm">Browse</button>
+                  <span className="text-center">Drag and drop</span>
+                </div>
+              </div>
+              {productImage.map((image, index)=> (
+                  <div className="size-32 rounded-lg" key={index}>
+                      <button className="btn btn-error size-32 opacity-0 hover:opacity-70 absolute active:ring ring-opacity-100 ring-error" onClick={()=> handleRemoveImage(index)}>
+                        <span className="material-icons">delete</span>
+                      </button>
+                      <img className="object-cover size-full rounded-lg" src={image} />
+                  </div>
+              ))}
+        </div>
           <label className="input input-bordered flex items-center gap-2">
             Name
             <input required name="name" className="grow bg-transparent" onChange={handleName}/>
@@ -95,7 +140,6 @@ function AddProductPage() {
               ))}
             </select>
           </div>
-          
           <div className="flex flex-row justify-between gap-5">
             <div className="form-control w-52">
               <label className="cursor-pointer label">
@@ -109,20 +153,6 @@ function AddProductPage() {
                   <input required name="Time" type="datetime-local" className="grow bg-transparent" onChange={handleTime} />
                 </label>
               )}
-          </div>
-          <div className="flex flex-wrap justify-center gap-3">
-                {productImage.map((image, index)=> (
-                    <div className="size-28 rounded-lg" key={index}>
-                        <button className="btn btn-error size-28 opacity-0 hover:opacity-70 absolute active:ring ring-opacity-100 ring-error" onClick={()=> handleRemoveImage(index)}>
-                          <span className="material-icons">delete</span>
-                        </button>
-                        <img className="object-cover size-full rounded-lg" src={image} />
-                    </div>
-                ))}
-          </div>
-          <div className="join w-full">
-              <input required name="imageUrl" type="text" className="input input-bordered w-full join-item" placeholder="Image url" onChange={handleImageField} />
-              <button className="btn btn-outline join-item" onClick={handleAddImage}>Add this image</button>
           </div>
           <button className="btn btn-primary btn-block" onClick={handleSubmit}>Add Product</button>
         </div>
