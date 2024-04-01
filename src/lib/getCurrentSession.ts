809a25fc4316f7eab18bd140ce071a439a7bbd20
Thitiphatx <thitiphat6391@gmail.com>
2024-04-01 17:10:27 +0700

    Redirect on logout

--- src/lib/getCurrentSession.ts
+++ src/lib/getCurrentSession.ts
@@ -1,7 +1,20 @@
 import { authOption } from "@/app/api/auth/[...nextauth]/authoption";
 import { getServerSession } from "next-auth";
+import { User } from "@prisma/client";
+
 
 export async function getCurrentSession() {
     const session = await getServerSession(authOption);
+    const data: User = {
+        id: session?.user.id ?? "",
+        name: session?.user.name ?? "",
+        address: session?.user.address,
+        email: "",
+        hashedPassword: "",
+        phone: "",
+        picture: "",
+        score: 0,
+        role: ""
+    }
     return session
 }
