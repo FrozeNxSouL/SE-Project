e4c312984e6964342151fefe3b25a4328d45cb58
Thitiphatx <thitiphat6391@gmail.com>
2024-04-02 18:28:20 +0700

    Remove image limit

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
     },
     reactStrictMode: false,
     output: 'standalone',
+    async rewrites() {
+        return [
+        {
+            source: '/api/:path*',
+            destination: 'http://localhost:3000/api/:path*', // Proxy to Backend
+        },
+        ]
+    },
+    api: {
+        bodyParser: {
+        sizeLimit: '10mb', // Adjust the size limit as needed, for example '10mb'
+        },
+    },
 };
 
 module.exports = nextConfig;
