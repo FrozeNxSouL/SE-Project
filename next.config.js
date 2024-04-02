1b486d58243659c3134af530760e825041433582
Thitiphatx <thitiphat6391@gmail.com>
2024-04-02 18:36:43 +0700

    Wow

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const nextConfig = {
     },
     reactStrictMode: false,
     output: 'standalone',
-    async rewrites() {
-        return [
-        {
-            source: '/api/:path*',
-            destination: 'http://localhost:3000/api/:path*', // Proxy to Backend
-        },
-        ]
-    },
-    api: {
-        bodyParser: {
-        sizeLimit: '10mb', // Adjust the size limit as needed, for example '10mb'
-        },
-    },
 };
 
 module.exports = nextConfig;
